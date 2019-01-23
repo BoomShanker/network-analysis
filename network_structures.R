8a1453b55f7f3d1a2caa155a64c0c84cf75166a4
Jered <jeredjsanchez@gmail.com>
2019-01-23 12:36:50 -0700

    additional development work

---
+++ network_structures.R
@@ -0,0 +1,173 @@
+
+# Load packages
+
+library(tidyverse)
+library(igraph)
+library(threejs)
+
+# Load gump.csv data file into a dataframe
+gump <- read_csv("data/gump.csv")
+
+# Inspect Forrest Gump Movie dataset
+head(gump)
+
+# Make an undirected network
+g <- graph_from_data_frame(gump, directed = FALSE)
+
+# Identify key nodes using eigenvector centrality
+g.ec <- eigen_centrality(g)
+which.max(g.ec$vector)
+
+# Plot Forrest Gump Network
+plot(g,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.6,
+     vertex.size = 25*(g.ec$vector),
+     edge.color = 'gray88',
+     main = "Forrest Gump Network"
+)
+
+# Get density of a graph
+gd <- edge_density(g)
+
+# Get the diameter of the graph g
+diameter(g, directed = FALSE)
+
+# Get the average path length of the graph g
+g.apl <- mean_distance(g, directed = FALSE)
+
+
+# Create one random graph with the same number of nodes and edges as g
+g.random <- erdos.renyi.game(n = gorder(g), p.or.m = gd, type = "gnp")
+
+g.random
+
+plot(g.random)
+
+# Get density of new random graph `g.random`
+edge_density(g.random)
+
+#Get the average path length of the random graph g.random
+mean_distance(g.random, directed = FALSE)
+
+
+# Generate 1000 random graphs
+gl <- vector('list',1000)
+
+for(i in 1:1000){
+  gl[[i]] <- erdos.renyi.game(n = gorder(g), p.or.m = gd, type = "gnp")
+}
+
+# Calculate average path length of 1000 random graphs
+gl.apls <- unlist(lapply(gl, mean_distance, directed = FALSE))
+
+# Plot the distribution of average path lengths
+hist(gl.apls, xlim = range(c(1.5, 6)))
+abline(v = g.apl, col = "red", lty = 3, lwd = 2)
+
+# Calculate the proportion of graphs with an average path length lower than our observed
+mean(gl.apls < g.apl)
+
+# Show all triangles in the network.
+matrix(triangles(g), nrow = 3)
+
+# Count the number of triangles that vertex "BUBBA" is in.
+count_triangles(g, vids='BUBBA')
+
+# Calculate  the global transitivity of the network.
+g.tr <- transitivity(g)
+g.tr
+
+# Calculate the local transitivity for vertex BUBBA.
+transitivity(g, vids='BUBBA', type = "local")
+
+# Calculate average transitivity of 1000 random graphs
+gl.tr <- lapply(gl, transitivity)
+gl.trs <- unlist(gl.tr)
+
+# Get summary statistics of transitivity scores
+summary(gl.trs)
+
+# Calculate the proportion of graphs with a transitivity score higher than Forrest Gump's network
+mean(gl.trs > g.tr)
+
+# Identify the largest cliques in the network
+largest_cliques(g)
+
+
+# Determine all maximal cliques in the network and assign to object 'clq'
+clq <- max_cliques(g)
+
+# Calculate the size of each maximal clique.
+table(unlist(lapply(clq, length)))
+
+# Assign largest cliques output to object 'lc'
+lc <- largest_cliques(g)
+
+# Create two new undirected subgraphs, each containing only the vertices of each largest clique.
+gs1 <- as.undirected(induced_subgraph(g, lc[[1]]))
+gs2 <- as.undirected(induced_subgraph(g, lc[[2]]))
+
+
+# Plot the two largest cliques side-by-side
+
+par(mfrow=c(1,2)) # To plot two plots side-by-side
+
+plot(gs1,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.9,
+     vertex.size = 0,
+     edge.color = 'gray28',
+     main = "Largest Clique 1",
+     layout = layout.circle(gs1)
+)
+
+plot(gs2,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.9,
+     vertex.size = 0,
+     edge.color = 'gray28',
+     main = "Largest Clique 2",
+     layout = layout.circle(gs2)
+)
+
+
+
+
+friends1_nodes <- read_csv("data/friends1_nodes.csv")
+friends1_edges <- read_csv("data/friends1_edges.csv")
+
+# Create an igraph object with attributes directly from dataframes
+g1 <- graph_from_data_frame(d = friends1_edges, vertices = friends1_nodes, directed = FALSE)
+
+# Plot the network
+plot(g1)
+
+# Convert the gender attribute into a numeric value
+values <- as.numeric(factor(V(g1)$gender))
+
+# Calculate the assortativity of the network based on gender
+assortativity(g1, values)
+
+# Calculate the assortativity degree of the network
+assortativity.degree(g1, directed = FALSE)
+
+# Calculate the observed assortativity
+observed.assortativity <- assortativity(g1, values)
+
+# Calculate the assortativity of the network randomizing the gender attribute 1000 times
+results <- vector('list', 1000)
+for(i in 1:1000){
+  results[[i]] <- assortativity(g1, sample(values))
+}
+
+# Plot the distribution of assortativity values and add a red vertical line at the original observed value
+hist(unlist(results))
+abline(v = observed.assortativity, col = "red", lty = 3, lwd=2)
+
+
+
+
+
+
+
